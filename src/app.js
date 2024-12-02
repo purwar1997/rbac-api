d1e6cff55662d98b92dd44b6903848aaf0655b0b
purwar1997 <shubhampurwar511@gmail.com>
2024-12-02 07:46:34 +0530

    feat: enable CORS on all API endpoints

--- src/app.js
+++ src/app.js
@@ -1,8 +1,10 @@
 import express from 'express';
+import cors from 'cors';
 import cookieParser from 'cookie-parser';
 import setupLogger from './utils/setupLogger.js';
 import setupSwagger from './utils/setupSwagger.js';
 import apiRouter from './routes/index.js';
+import corsOptions from './utils/corsOptions.js';
 import { errorHandler } from './middlewares/errorHandler.js';
 
 const app = express();
@@ -13,6 +15,7 @@ setupSwagger(app);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
+app.use(cors(corsOptions));
 app.use('/api', apiRouter);
 app.use(errorHandler);
 
---
+++ src/utils/corsOptions.js
@@ -0,0 +1,13 @@
+const whitelist = ['http://api.rbac.shubhampurwar.in'];
+
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (whitelist.includes(origin) || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+};
+
+export default corsOptions;
